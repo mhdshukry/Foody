54c46f745f9bffc10ef1d1bff772994d4cfd0864
MOHAMED SHUKRY <mhdshukry110@gmail.com>
2023-09-07 23:20:25 +0530

    Full Code

---
+++ javasc.js
@@ -0,0 +1,34 @@
+
+var swiper = new Swiper(".container", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+});
+
+
+// var swiper = new Swiper(".review-slider", {
+//     slidesPerView: 3,
+//     centeredSlides: true,
+//     spaceBetween: 30,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         type: "fraction",
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
